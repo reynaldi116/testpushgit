9b42e3001dea0bd40e7f11c9c8236848dbbba84a
reynaldi116 <145915514+reynaldi116@users.noreply.github.com>
2025-05-30 16:41:41 +0700

    Delete utils directory

--- utils/filters.py
+++
@@ -1,6 +0,0 @@
-from scipy.signal import butter, filtfilt
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
-    nyq = 0.5 * fs
-    b, a = butter(order, [lowcut / nyq, highcut / nyq], btype='band')
-    return filtfilt(b, a, [data])[-1]
